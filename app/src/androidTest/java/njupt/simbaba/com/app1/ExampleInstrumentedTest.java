f4c3000461880486b9ac666fd0727392433774e9
Simbaba <zhaoxi.du@gmail.com>
2018-06-02 21:36:25 +0800

    add espresso unit test sample

--- app/src/androidTest/java/njupt/simbaba/com/app1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/njupt/simbaba/com/app1/ExampleInstrumentedTest.java
@@ -2,12 +2,22 @@ package njupt.simbaba.com.app1;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import njupt.simbaba.com.applib.A1;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+
 import static org.junit.Assert.*;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +26,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<A1> vmActivityRule = new ActivityTestRule<>(A1.class);
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("njupt.simbaba.com.app1", appContext.getPackageName());
+    }
+
+    /**
+     * https://developer.android.google.cn/training/testing/espresso/basics
+     */
+    @Test
+    public void checkActivityIdTest() {
+        onView(withId(R.id.name_of_activity)).check(matches(isDisplayed()));
+        onView(withId(R.id.name_of_activity)).check(matches(withSubstring("1000")));
     }
 }
